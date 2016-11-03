c07772e777c41d3b10cfff35e8609a2998c25d75
Daniel Pope <mauve@mauveweb.co.uk>
2016-11-03 20:44:54 +0000

    Add more rooms

--- rooms.py
+++ rooms.py
@@ -23,12 +23,37 @@ border. Silver cutlery is laid in 7 places for dinner... though one will not
 be joining them.
 """
 
+conservatory = Item('conservatory')
+conservatory.description = """\
+Through the tall, narrow panels of the conservatory's glass, you can see that
+it is now dark. Any warmth this winter day had has bled away and it feels
+distinctly chilly. However, many of the conservatory's plants are bathed in
+the warmth of electric lightbulbs.
+"""
+
+hall = Item('hall')
+hall.description = """\
+The hall is a long, bare room, with a marble floor and impressive staircase
+carpeted in red. A long table punctuates the centre of the room, and holds
+an elaborate spray of white lilies.
+"""
+
+study = Item('study')
+study.description = """\n
+The study's dark oak desk is strewn with paperwork, mostly letters.
+
+A fire burns dimly on the west wall. Beside it, an untouched glass of sherry
+awaits a man who will never again drink it.
+"""
 
 
 rooms = Bag({
     kitchen,
     library,
-    dining_room
+    dining_room,
+    conservatory,
+    hall,
+    study
 })
 
 
@@ -36,4 +61,4 @@ rooms = Bag({
 def list_rooms():
     print("Albermore Manor is a Victorian-era house with:")
     for r in rooms:
-        print(r)
+        print('A', r)
